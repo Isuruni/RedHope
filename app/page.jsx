72f418e0eb2f93dde0b49baf100a928397711105
Rajamiththiran <rjmithu7@gmail.com>
2023-09-19 15:18:03 +0530

    Raw Navbar added

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,28 @@
+import Feed from "@/components/Feed";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <section className="w-full flex-col flex-center">
+      <h1 className="head_text text-center">
+        Discover & Connect
+        <br className="max-md:hidden" />
+        <span className="red_gradient text-center">
+          {" "}
+          Connect Donors and Requestors{" "}
+        </span>
+      </h1>
+      <p className="desc text-center">
+        RedHope is an online platform dedicated to bridging the critical gap
+        between blood donors and those in need of blood donations. This platform
+        serves as a lifeline for individuals and communities, facilitating the
+        crucial process of connecting donors with requestors in a seamless and
+        efficient manner.
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
-export default page;
+export default Home;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Nav = () => {
-  return <div>NavBar</div>;
+  const { data: session } = useSession();
+
+  const user = session?.user;
+
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3 pr-0">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          width={60}
+          height={60}
+          className="object-contain"
+        />
+        <p className="logo_text">ReadHope</p>
+      </Link>
+      {/* desktop Nav // we have to add user means is there any user there wrap whole react */}
+      <div className="sm:flex hidden">
+        {user ? (
+          <div className="flex gap-3 md:gap-5">
+            <button type="button" onClick={signOut} className="red_btn">
+              Sign Out
+            </button>
+            <Link href="/request-blood" className="red_btn">
+              Request Blood
+            </Link>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/profile.png"
+                width={37}
+                height={37}
+                className=" rounded-full "
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <React.Fragment>
+            <button
+              type="button"
+              //key={provider.name}
+              handleButtonClick={() => {}}
+              className="red_btn mr-[10px]"
+            >
+              Sign Up
+            </button>
+            <Link href="/request-blood" className="red_btn">
+              Request Blood
+            </Link>
+          </React.Fragment>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
